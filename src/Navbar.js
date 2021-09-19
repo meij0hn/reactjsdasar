482db4e81e63d93af36195572d6358122d4cb0d2
Muhammad Meijohn <meijohnnura@gmail.com>
2021-09-19 09:34:02 +0700

    20210919_0934

---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+//Class Component
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Navbar</h3>
+//             </div>
+//         )
+//     }
+// }
+
+// Function Component
+// import React from 'react'
+
+// export default function Navbar() {
+//     return (
+//         <div>
+//             <h3>Navbar</h3>
+//         </div>
+//     )
+// }
+
+//Arrow  Function
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>Navbar</h3>
+        </div>
+    )
+}
+
+export default Navbar
